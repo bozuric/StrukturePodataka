ce7fa526215202df60964616e6f802a35506c6fb
bozuric <jbozur00@fesb.hr>
2024-10-14 16:48:00 +0200

    zad1.c

--- zad1.c
+++ zad1.c
@@ -1,10 +1,3 @@
-﻿//Napisati program koji prvo pročita koliko redaka ima datoteka, 
-//tj.koliko ima studenata zapisanih u datoteci. Nakon toga potrebno 
-//je dinamički alocirati prostor za niz strukturastudenata(ime, prezime, bodovi) 
-//i učitati iz datoteke sve zapise.Na ekran ispisati ime, prezime, apsolutni i
-//relativni broj bodova.Napomena: Svaki redak datoteke sadrži ime i prezime studenta, 
-//te broj bodova na kolokviju.relatvan_br_bodova = br_bodova / max_br_bodova * 100
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
